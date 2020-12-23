5264b2dbe08e410767558f72e3903e331a0e6e55
Sam Ayler <sayler8701@gmail.com>
2020-12-23 14:19:48 -0800

    adding switch case for user action

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const inquirer = require("inquirer");
 //Starting connection to mysql
 const connection = mysql.createConnection({
 	host: "localhost",
-
 	port: 3306,
-
 	user: "root",
-
 	password: "password",
 	database: "employees_db",
 });
@@ -17,3 +14,51 @@ connection.connect((err) => {
 	if (err) throw err;
 	startProgram();
 });
+
+const startProgram = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all roles',
+            'View all departments',
+            'Add department',
+            'Add role',
+            'Add employee',
+            'Update employee role',
+            'EXIT',
+        ],
+    })
+    .then(({ action }) => {
+        console.log(action);
+        switch (action) {
+            case 'View all employees':
+                
+                break;
+            case 'View all roles':
+                
+                break;
+            case 'View all departments':
+                
+                break;
+            case 'Add department':
+                
+                break;
+            case 'Add role':
+                
+                break;
+            case 'Add employee':
+                
+                break;
+            case 'Update employee role':
+                
+                    break;
+            default:
+                connection.end();
+                process.exit(0);
+                break;
+        }
+    });
+};
