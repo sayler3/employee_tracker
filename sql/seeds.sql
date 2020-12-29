9067558f5dd164c0cc4431962bcd2d4706fc3ecd
Sam Ayler <sayler8701@gmail.com>
2020-12-29 11:31:15 -0800

    updating directory sql and adding seeds.sql

---
+++ sql/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ('Legal'), ('UX'), ('Development'), ('Marketing'), ('OPS');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Director', 150000, 1), ('Advisor', 145000, 2), 
+('UX Manager', 165000, 2), ('Senior Dev', 150000, 3),
+('Dev', 120000, 3), ('Senior Sales', 110000, 4), ('Sales', 95000, 4),
+('HR', 98000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Harry', 'Potter', 4), ('Ron', 'Weasley', 5, 1), 
+('Hermione', 'Granger', 1), ('Lord', 'Voldemont' 5, 1),
+('Luna', 'Lovegood', 8), ('Albus', 'Dumbledore', 6),
+('Pansy', 'Parkinson', 3), ('Sirus', 'Black', 2, 7),
+('Neville', 'Longbottom', 7, 6);
