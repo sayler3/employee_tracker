a374c99f2fb179d9a485b7dc8841f9ed16ebddc7
Sam Ayler <sayler8701@gmail.com>
2020-12-29 21:02:27 -0800

    update seeds.sql

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -7,9 +7,9 @@ VALUES ('Director', 150000, 1), ('Advisor', 145000, 2),
 ('Dev', 120000, 3), ('Senior Sales', 110000, 4), ('Sales', 95000, 4),
 ('HR', 98000, 5);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ('Harry', 'Potter', 4), ('Ron', 'Weasley', 5), 
-('Hermione', 'Granger', 1), ('Lord', 'Voldemont', 5),
-('Luna', 'Lovegood', 8), ('Albus', 'Dumbledore', 6),
-('Pansy', 'Parkinson', 3), ('Sirus', 'Black', 2),
-('Neville', 'Longbottom', 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Harry', 'Potter', 4, null), ('Ron', 'Weasley', 5, 1), 
+('Hermione', 'Granger', 1, null), ('Lord', 'Voldemont', 5, 1),
+('Luna', 'Lovegood', 8, null), ('Albus', 'Dumbledore', 6, null),
+('Pansy', 'Parkinson', 3, null), ('Sirus', 'Black', 2, 7),
+('Neville', 'Longbottom', 7, 6);
