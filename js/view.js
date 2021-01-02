622f0e2103be5a83c9ff358cef6cc5b88f813c3b
Sam Ayler <sayler8701@gmail.com>
2021-01-02 10:00:36 -0800

    updated view.js view all employee function to show correct information

--- js/view.js
+++ js/view.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
 	host: "localhost",
@@ -10,27 +10,33 @@ const connection = mysql.createConnection({
 });
 
 module.exports = {
-    viewAllEmployees: function (callBack) {
-        connection.query('SELECT * FROM employee', (err, results) => {
-            if (err) throw err;
-            console.table(results)
-            callBack(results);
-        });
-    },
+	viewAllEmployees: function (callBack) {
+		connection.query(
+			'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name as department, concat(manager.first_name, " ", manager.last_name) as Manager \
+            FROM employee as employee INNER JOIN role as role on employee.role_id = role.id \
+            INNER JOIN department as department ON role.department_id = department.id \
+            LEFT JOIN employee as manager on employee.manager_id = manager.id ORDER BY employee.id;',
+			(err, results) => {
+				if (err) throw err;
+				console.table(results);
+				callBack(results);
+			}
+		);
+	},
 
-    viewAllRoles: function (callBack) {
-        connection.query('SELECT * FROM role', (err, results) => {
-            if (err) throw err;
-            console.table(results)
-            callBack(results);
-        });
-    },
+	viewAllRoles: function (callBack) {
+		connection.query("SELECT * FROM role", (err, results) => {
+			if (err) throw err;
+			console.table(results);
+			callBack(results);
+		});
+	},
 
-    viewAllDepartments: function (callBack) {
-        connection.query('SELECT * FROM department', (err, results) => {
-            if (err) throw err;
-            console.table(results)
-            callBack(results);
-        });
-    }
-}
+	viewAllDepartments: function (callBack) {
+		connection.query("SELECT * FROM department", (err, results) => {
+			if (err) throw err;
+			console.table(results);
+			callBack(results);
+		});
+	},
+};
