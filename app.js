1b5f32465c02dc37a3dd5f296425082b621a2221
Sam Ayler <sayler8701@gmail.com>
2021-01-06 12:39:26 -0800

    update for heroku

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const express = require('express');
 const app = express();
 
 app.listen(process.env.PORT || 8080);
+app.get('*', function(req, res)
 
 //Starting connection to mysql
 // const connection = mysql.createConnection({
